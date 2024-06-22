277b42f9e82a5b43b73efc78d37980a475e1defb
Endkind <endkind.ender@gmail.com>
2024-06-22 16:23:42 +0200

    init

---
+++ src/main/java/net/endkind/enderfly/EnderFly.java
@@ -0,0 +1,17 @@
+package net.endkind.enderfly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderFly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
