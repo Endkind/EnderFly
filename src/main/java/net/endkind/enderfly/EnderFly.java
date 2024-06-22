76ff47ed35cefbd42e56a6e447d4cffe2f4dc899
Endkind <endkind.ender@gmail.com>
2024-06-22 16:33:50 +0200

    add /fly machanics

--- src/main/java/net/endkind/enderfly/EnderFly.java
+++ src/main/java/net/endkind/enderfly/EnderFly.java
@@ -7,11 +7,19 @@ public final class EnderFly extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Enabling EnderFly");
+        if (getCommand("enderfly") != null) {
+            getCommand("enderfly").setExecutor(new CommandHandler());
+        } else {
+            getLogger().severe("Command 'enderfly' could not be found!");
+        }
+        getLogger().info("EnderFly is enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Disabling EnderFly");
+        getLogger().info("EnderFly is disabled");
     }
 }
